6a94ceb100356028da0e3400e0d30d1adda46db8
AggMtt <agustinmateoar@gmail.com>
2023-02-09 21:55:48 -0300

    Add homeworks

---
+++ Java.02/Definiciones-02.js
@@ -0,0 +1,9 @@
+Definiciones 02-js
+Variables: Son definiciones a las cuales les podemos agregar un valor.
+Strings: Valor de la variable.
+Funciones: Son una especie de caja virtual que agrupa códigos.
+Funciones-Argumentos: Son funciones a las que se les suele dar una acción y como responder ante ella.
+Funciones- Return: Retorna lo que acordemos nosotros. Solo podemos retornar una vez, por lo que, todo codigo que se encuentre debajo de el no se podra leer.
+Declaraciones if: Se ejecuta de maneras diferentes segun las condiciones que cumpla.
+Boolean(true/false): Mediante código binario se le dara el valor correcto, el cual puede ser verdadero o falso.
+
